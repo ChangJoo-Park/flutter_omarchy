c7143cc9df55e2639c1f4e78cbbdab1c149975b6
aloisdeniel <alois.deniel@outlook.com>
2025-08-23 22:22:07 +0000

    Deploying to gh-pages from @ aloisdeniel/flutter_omarchy@7dad85ac225b8a66eb6a34ff0defab80a2b6fdbd 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5893372067e620ae551be4beda1fbab3",
+const RESOURCES = {"flutter_bootstrap.js": "053503b6fa81d77a05c0c01275366ccd",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
@@ -24,9 +24,9 @@ const RESOURCES = {"flutter_bootstrap.js": "5893372067e620ae551be4beda1fbab3",
 "canvaskit/skwasm_heavy.js.symbols": "3c01ec03b5de6d62c34e17014d1decd3",
 "canvaskit/canvaskit.wasm": "07b9f5853202304d3b0749d9306573cc",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
-"main.dart.wasm": "6ec3926222e558871c079c13676cf35c",
+"main.dart.wasm": "c1c7c1c8b2eed87b2e3ac46c7b6a4127",
 "main.dart.mjs": "39159bad3a2a23d1b5f1c3c93854f92b",
-"main.dart.js": "469cb894db8b78b3f351b57a63fb1d1f",
+"main.dart.js": "c32e5831db2c84976cc6f1cd6a6db01f",
 "index.html": "5c2e245a6e331442f4d13eb3f72e0d68",
 "/": "5c2e245a6e331442f4d13eb3f72e0d68",
 "assets/packages/flutter_omarchy/lib/fonts/CaskaydiaMonoNerdFontMono-Bold.ttf": "31449a5451ae963f4799540029618430",
