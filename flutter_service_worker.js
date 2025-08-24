a4121a55af93070b2cc9ec0b4fba47cf13c2042a
aloisdeniel <alois.deniel@outlook.com>
2025-08-24 13:36:22 +0000

    Deploying to gh-pages from @ aloisdeniel/flutter_omarchy@890feef2d1c8082ba29a1f94cc511951df784243 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "420a02b0787176806f84b7d09f8aa37b",
+const RESOURCES = {"flutter_bootstrap.js": "7266c62f5646f5a7720654e0153df97e",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
